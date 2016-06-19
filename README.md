405c57979c817471086fb99b96e193e215962b73
Gwenael Lambrouin <gwenael.lambrouin@gmail.com>
2016-06-19 22:19:45 +0200

    Display (empty) root window

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # abcde
 abcde is an editor to write ABC music files
+
+abcde is work in progress. It is unusable at the moment.
--- snap.py
+++ snap.py
@@ -1,4 +1,5 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from pyfluidsynth3 import fluidaudiodriver, fluidhandle, fluidsettings, fluidsynth
 import time
