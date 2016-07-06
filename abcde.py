2ca8d4ae22da5b884f9692c1ea1fdc548f498c82
Gwenael Lambrouin <gwenael.lambrouin@gmail.com>
2016-07-06 22:59:50 +0200

    Do not play a note when editing inside a comment

--- abcde.py
+++ abcde.py
@@ -69,13 +69,17 @@ class EditZone(tkinter.Frame):
             self.alt = True
         elif not self.control and not self.alt and \
                         event.keysym.upper() in snap.c_major_scale:
-            self.snap.play_abc_note(event.keysym)
-        elif event.keysym in ['t']: # Test!
-            print("Current cursor position: " + self.edit_zone.index(tkinter.INSERT))
+            note = self.get_note_to_play(event.keysym)
+            if note is not None:
+                self.snap.play_abc_note(event.keysym)
 
-            print(self.edit_zone.get('1.0'))
-            print(self.edit_zone.get(tkinter.INSERT))
-            print(self.edit_zone.get('1.0', tkinter.END))
+        elif event.keysym in ['t']: # Test!
+            self.get_cur_line_to_insert()
+#            print("Current cursor position: " + self.edit_zone.index(tkinter.INSERT))
+#
+#            print(self.edit_zone.get('1.0'))
+#            print(self.edit_zone.get(tkinter.INSERT))
+#            print(self.edit_zone.get('1.0', tkinter.END))
 
     def on_key_release(self, event):
         if event.keysym in ['Shift_R','Shift_L']:
@@ -84,6 +88,47 @@ class EditZone(tkinter.Frame):
             self.control = False
         elif event.keysym in ['Alt_L']:
             self.alt = False
+
+    def get_cur_line_to_insert(self):
+        """Get the current line of text from the beginning of the line to the
+        insertion point (text cursor).
+
+        :return: a string
+        """
+        insert_index = self.edit_zone.index(tkinter.INSERT)
+        (cur_line, cur_col) = insert_index.split('.')
+        line_start_index = '{}.{}'.format(cur_line, 0)
+        cur_line_to_insert = self.edit_zone.get(line_start_index, insert_index)
+        return cur_line_to_insert
+
+    def get_note_to_play(self, keysym):
+        """Given a keysym following a key press, check whether there is a
+         note to play. If so, return the note.
+
+         :param keysym The key pressed
+
+         :return a String with the note to play in ABC-normalized format, or None
+         if there is no note to play. The note is absolute, ie not relative to
+         the tune scale.
+         """
+
+        # Check whether we are in comment context
+        cur_line_to_insert = self.get_cur_line_to_insert()
+        if cur_line_to_insert.find('%') != -1:
+            # There is a '%' before the text cursor => we are in comment context
+            return None
+
+        # Check whether we are in informative field context
+
+        # Find the tune key (default to C)
+
+        # Get the note to play with all the useful attributes (accidentals,
+        # octave changes, ...)
+
+        # Normalize the note
+
+
+        return keysym
 
 
 root = tkinter.Tk()
